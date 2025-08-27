3f53133f87f15ade4d2151d2f5dea07901fedefc
belafrata <fratabela@gmail.com>
2025-08-27 20:57:15 -0300

    terceiro commit

--- src/js/script.js
+++ src/js/script.js
@@ -26,28 +26,28 @@ const perguntas = [
     {
         pergunta: "Você está em casa e o telefone toca. Uma voz mascarada começa a fazer perguntas bizarras e ameaçadoras. O que você faz?",
         respostas: [
-            { id: 1, text: "Tenta conversar para ganhar tempo.", correct:false},
-            { id: 2, text: "Desliga e vai atrás de armas.", correct:false},
-            { id: 3, text: "Desliga imediatamente e chama a polícia.", correct:true},
-            { id: 4, text: "Ignora e acha que é brincadeira.", correct:false},
+            { id: 1, text: "Tenta conversar para ganhar tempo.", correct:1},
+            { id: 2, text: "Desliga e vai atrás de armas.", correct:0},
+            { id: 3, text: "Desliga imediatamente e chama a polícia.", correct:2},
+            { id: 4, text: "Ignora e acha que é brincadeira.", correct:0},
         ]
     },
     {
         pergunta: "Durante uma festa, as luzes se apagam repentinamente e começam a aparecer mensagens ameaçadoras nas paredes. Qual a sua reação?",
         respostas: [
-            { id: 1, text: "Encaro a situação com coragem e vou atrás do culpado.", correct:false},
-            { id: 2, text: "Procuro a saída mais próxima em silêncio.", correct:true},
-            { id: 3, text: "Fico parado, assustado.", correct:false},
-            { id: 4, text: "Tento juntar um grupo para se proteger.", correct:false},
+            { id: 1, text: "Encaro a situação com coragem e vou atrás do culpado.", correct:0},
+            { id: 2, text: "Procuro a saída mais próxima em silêncio.", correct:2},
+            { id: 3, text: "Fico parado, assustado.", correct:0},
+            { id: 4, text: "Tento juntar um grupo para se proteger.", correct:1},
         ]
     },
     {
         pergunta: "Alguém está te seguindo até sua casa à noite e você percebe que está com o celular sem bateria. O que você faz?",
         respostas: [
-            { id: 1, text: "Tento encontrar um lugar seguro na vizinhança.", correct:false},
-            { id: 2, text: "Chamo ajuda gritando.", correct:false},
-            { id: 3, text: "Enfrento o perseguidor.", correct:false},
-            { id: 4, text: "Corro para dentro e tranco todas as portas.", correct:true},
+            { id: 1, text: "Tento encontrar um lugar seguro na vizinhança.", correct:1},
+            { id: 2, text: "Chamo ajuda gritando.", correct:0},
+            { id: 3, text: "Enfrento o perseguidor.", correct:0},
+            { id: 4, text: "Corro para dentro e tranco todas as portas.", correct:2},
         ]
     },
 ]
@@ -90,27 +90,43 @@ function mostrarPergunta() {
 }
 
 function selectAnswer(e) {
-    respostas = perguntas[perguntaAtuaIndex].respostas;
-    const respostaCorreta = respostas.filter((resposta) => resposta.correct == true)[0];
-
+    const respostas = perguntas[perguntaAtuaIndex].respostas;
     const selectedBtn = e.target;
-    const isCorrect = selectedBtn.dataset.id == respostaCorreta.id;
-    if (isCorrect) {
+    const selectedId = Number(selectedBtn.dataset.id);
+    const respostaSelecionada = respostas.find(r => r.id === selectedId);
+    if (respostaSelecionada.correct === 2) {
         selectedBtn.classList.add("correto");
-        pontuacao++;
-    }
+        pontuacao += 2;
+    } 
+    else if (respostaSelecionada.correct === 1) {
+        selectedBtn.classList.add("meio-correto");
+        pontuacao += 1;
+    } 
     else {
         selectedBtn.classList.add("incorreto");
     }
+
     Array.from(botaoResposta.children).forEach((botao) => {
         botao.disabled = true;
     });
+
     botaoProximo.style.display = "block";
 }
 
+
 function mostrarPontuacao() {
     resetState();
-    perguntaElemento.innerHTML = `Você acertou ${pontuacao} de ${perguntas.length}!`;
+    let resultadoTexto = "";
+
+    if (pontuacao === perguntas.length * 2) {
+        resultadoTexto = "Parabéns! Você não seria uma vítima!";
+    } else if (pontuacao >= 2 && pontuacao < perguntas.length * 2) {
+        resultadoTexto = "Cuidado! Talvez você fosse uma vítima…";
+    } else {
+        resultadoTexto = "Infelizmente, você seria a próxima vítima!";
+    }
+
+    perguntaElemento.innerHTML = resultadoTexto;
     botaoProximo.innerHTML = "Jogar novamente";
     botaoProximo.style.display = "block";
 }
